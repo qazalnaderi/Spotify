1b4746cbdd3da28a93ab88c028e15dfd8dd2ab8c
qazalnaderi <ghazalnaderi4@gmail.com>
2025-02-25 19:37:44 +0330

    library id added tp schema nad user model

--- backend/services/core-service/app/domain/schemas/user_schema.py
+++ backend/services/core-service/app/domain/schemas/user_schema.py
@@ -3,11 +3,12 @@ from datetime import date, datetime
 from typing import Optional
 
 class UserInfoSchema(BaseModel):
+    user_id: int
+    library_id: int
     first_name: str
     last_name: str
     email: str
     username: str
-
     class Config:
         from_attributes = True
 
